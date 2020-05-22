078ec0f9f2fd757a3079017d68f6aa83447d6bc4
Gian Rothfuchs <gian@Gians-MacBook-Pro.local>
2020-05-22 17:29:19 +0200

    ttt added

--- ttt.py
+++ ttt.py
@@ -1,188 +1,238 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar  2 21:53:08 2018
+Created on Wed Feb 19 21:39:18 2020
 
-@author: gian
+@author: gian rothfuchs
 """
-"""
-Full code for running a game of tic-tac-toe on a board of any size with a specified number in a row for the win. This is
-similar to tic_tac_toe.py but all relevent moves are paramiterized by board_size arg that sets how big the board is and
-winning_length which determines how many in a row are needed to win. Defaults are 5 and 4. This allows you to play games
-in a more complex environment than standard tic-tac-toe.
-Two players take turns making moves on squares of the board, the first to get winning_length in a row, including
-diagonals, wins. If there are no valid moves left to make the game ends a draw.
-The main method to use here is play_game which simulates a game to the end using the function args it takes to determine
-where each player plays.
-The board is represented by a board_size x board_size tuple of ints. A 0 means no player has played in a space, 1 means
-player one has played there, -1 means the seconds player has played there. The apply_move method can be used to return a
-copy of a given state with a given move applied. This can be useful for doing min-max or monte carlo sampling.
-"""
-import random
-import itertools
 
+import numpy as np
+import random as rnd
+import itertools as it
+import collections as cl
 
-def _new_board(board_size):
-    """Return a emprty tic-tac-toe board we can use for simulating a game.
-    Args:
-        board_size (int): The size of one side of the board, a board_size * board_size board is created
-    Returns:
-        board_size x board_size tuple of ints
-    """
-    return tuple(tuple(0 for _ in range(board_size)) for _ in range(board_size))
-
-
-def apply_move(board_state, move, side):
-    """Returns a copy of the given board_state with the desired move applied.
-    Args:
-        board_state (2d tuple of int): The given board_state we want to apply the move to.
-        move (int, int): The position we want to make the move in.
-        side (int): The side we are making this move for, 1 for the first player, -1 for the second player.
-    Returns:
-        (2d tuple of int): A copy of the board_state with the given move applied for the given side.
-    """
-    move_x, move_y = move
-
-    def get_tuples():
-        for x in range(len(board_state)):
-            if move_x == x:
-                temp = list(board_state[x])
-                temp[move_y] = side
-                yield tuple(temp)
+class tictactoe:
+    def __init__(self,bsize,p1,p2,showField,verbose):
+        self.verbose = verbose
+        self.boardSize = bsize
+        self.p1 = p1
+        self.p2 = p2
+        self.name2num = {self.p1:1,self.p2:-1}
+        self.num2name = {1:self.p1,-1:self.p2}
+        self.transitionMap = {self.p1:self.p2,self.p2:self.p1}
+        self.stateHash2Num, self.stateNum2Str, self.stateSpace = self.getStateSpace()
+        self.resetBoard()
+        self.showField = showField
+        
+    
+        
+    def resetBoard(self):
+        self.board = np.zeros((self.boardSize,self.boardSize),dtype=np.int8)
+        self.gameOver = False
+        self.State = None
+        self.rewardVal = 0
+        self.emptyFields = self.boardSize**2
+        self.actionSpace = self.getEmptyFields()
+        self.randomStart()
+        
+    def randomStart(self):
+        playerToken = np.array([1,-1])
+        np.random.shuffle(playerToken)
+        self.currentPlayer = self.num2name[playerToken[0]]
+        if self.verbose: print(self.currentPlayer +" goes first")
+        
+    def getState(self):
+        self.State = tuple(self.board.reshape(self.boardSize**2))
+        return self.State 
+    
+    def getStateSpace(self):
+        dictsStateStr2Num = {}
+        dictStateNum2Str = {}
+        stateList = []
+        ctr = 0
+        ctrState = 0
+        for line in list(it.product(range(-1,2), repeat=self.boardSize**2)):
+                
+            ctr += 1
+            occurrencesDict = cl.Counter(line)
+            if occurrencesDict[1] <= int(round((self.boardSize**2+1)/2)) and occurrencesDict[-1] <= int(round((self.boardSize**2+1)/2)):
+                dictsStateStr2Num[tuple(line)] = ctrState
+                dictStateNum2Str[ctrState] = str(np.array(line))
+                stateList.append(ctrState)
+                ctrState += 1
+        return dictsStateStr2Num, dictStateNum2Str, stateList
+        
+    def getWinner(self):
+        col_p1 = np.any(np.equal(np.sum(self.board,0),np.full(self.boardSize, self.boardSize)))
+        row_p1 = np.any(np.equal(np.sum(self.board,1),np.full(self.boardSize, self.boardSize)))
+        diag1_p1 = np.equal(np.sum(np.multiply(self.board,np.eye(self.boardSize))),self.boardSize)
+        diag2_p1 = np.equal(np.sum(np.multiply(self.board,np.rot90(np.eye(self.boardSize)))),self.boardSize)
+        
+        col_p2 = np.any(np.equal(np.sum(self.board,0),np.full(self.boardSize, -self.boardSize)))
+        row_p2 = np.any(np.equal(np.sum(self.board,1),np.full(self.boardSize, -self.boardSize)))
+        diag1_p2 = np.equal(np.sum(np.multiply(self.board,np.eye(self.boardSize))),-self.boardSize)
+        diag2_p2 = np.equal(np.sum(np.multiply(self.board,np.rot90(np.eye(self.boardSize)))),-self.boardSize)
+        
+        #print(np.array([col_p1,row_p1,diag1_p1,diag2_p1]))
+        if np.any(np.array([col_p1,row_p1,diag1_p1,diag2_p1])):
+            self.gameOver = True
+            if self.verbose: print('GAME OVER: ' + self.p1 +' wins')
+            self.rewardVal = 1.0
+            return self.p1 # p1 wins
+        else:
+            #print(np.array([col_p2,row_p2,diag1_p2,diag2_p2]))
+            if np.any(np.array([col_p2,row_p2,diag1_p2,diag2_p2])):
+                self.gameOver = True
+                if self.verbose: print('GAME OVER: ' + self.p2 +' wins')
+                self.rewardVal = -1.0
+                return self.p2 #p2 wins
             else:
-                yield board_state[x]
-
-    return tuple(get_tuples())
-
-
-def available_moves(board_state):
-    """Get all legal moves for the current board_state. For Tic-tac-toe that is all positions that do not currently have
-    pieces played.
-    Args:
-        board_state: The board_state we want to check for valid moves.
-    Returns:
-        Generator of (int, int): All the valid moves that can be played in this position.
-    """
-    for x, y in itertools.product(range(len(board_state)), range(len(board_state[0]))):
-        if board_state[x][y] == 0:
-            yield (x, y)
-
-
-def _has_winning_line(line, winning_length):
-    count = 0
-    last_side = 0
-    for x in line:
-        if x == last_side:
-            count += 1
-            if count == winning_length:
-                return last_side
+                if self.emptyFields == 0:
+                    self.gameOver = True
+                    if self.verbose: print('GAME OVER: Tie ')
+                    self.rewardVal = 0.0
+                    return 'tie' # tie
+                else:    
+                    self.rewardVal = -0.01
+                    return 'na' # game in progres
+                
+    def getEmptyFields(self):
+        self.emptyFields = np.sum(self.board == 0)
+        if self.emptyFields > 0:
+            res = np.where(self.board == 0)
+            return list(zip(res[0], res[1]))
         else:
-            count = 1
-            last_side = x
-    return 0
-
-
-def has_winner(board_state, winning_length):
-    """Determine if a player has won on the given board_state.
-    Args:
-        board_state (2d tuple of int): The current board_state we want to evaluate.
-        winning_length (int): The number of moves in a row needed for a win.
-    Returns:
-        int: 1 if player one has won, -1 if player 2 has won, otherwise 0.
-    """
-    board_width = len(board_state)
-    board_height = len(board_state[0])
-
-    # check rows
-    for x in range(board_width):
-        winner = _has_winning_line(board_state[x], winning_length)
-        if winner != 0:
-            return winner
-    # check columns
-    for y in range(board_height):
-        winner = _has_winning_line((i[y] for i in board_state), winning_length)
-        if winner != 0:
-            return winner
-
-    # check diagonals
-    diagonals_start = -(board_width - winning_length)
-    diagonals_end = (board_width - winning_length)
-    for d in range(diagonals_start, diagonals_end+1):
-        winner = _has_winning_line(
-            (board_state[i][i + d] for i in range(max(-d, 0), min(board_width, board_height - d))),
-            winning_length)
-        if winner != 0:
-            return winner
-    for d in range(diagonals_start, diagonals_end+1):
-        winner = _has_winning_line(
-            (board_state[i][board_height - i - d - 1] for i in range(max(-d, 0), min(board_width, board_height - d))),
-            winning_length)
-        if winner != 0:
-            return winner
-
-    return 0  # no one has won, return 0 for a draw
-
-
-def play_game(plus_player_func, minus_player_func, board_size=5, winning_length=4, log=False):
-    """Run a single game of tic-tac-toe until the end, using the provided function args to determine the moves for each
-    player.
-    Args:
-        plus_player_func ((board_state(board_size by board_size tuple of int), side(int)) -> move((int, int))):
-            Function that takes the current board_state and side this player is playing, and returns the move the player
-            wants to play.
-        minus_player_func ((board_state(board_size by board_size tuple of int), side(int)) -> move((int, int))):
-            Function that takes the current board_state and side this player is playing, and returns the move the player
-            wants to play.
-        board_size (int): The size of a single side of the board. Game is played on a board_size*board_size sized board
-        winning_length (int): The number of pieces in a row needed to win a game.
-        log (bool): If True progress is logged to console, defaults to False
-    Returns:
-        int: 1 if the plus_player_func won, -1 if the minus_player_func won and 0 for a draw
-    """
-    board_state = _new_board(board_size)
-    player_turn = 1
-
-    while True:
-        _available_moves = list(available_moves(board_state))
-        if len(_available_moves) == 0:
-            # draw
-            if log:
-                print("no moves left, game ended a draw")
-            return 0.
-        if player_turn > 0:
-            move = plus_player_func(board_state, 1)
+            return list()
+    
+    
+    def doMove(self,field,player):
+        otpt = False
+        if self.currentPlayer == player:
+            if field in self.getEmptyFields():
+                if not self.gameOver:
+                    self.board[field] = self.name2num[self.currentPlayer]
+                    if self.showField:
+                        print(self.currentPlayer + " played: {0}".format(field))
+                        print(self.board)
+                    self.currentPlayer = self.transitionMap[self.currentPlayer]
+                    self.getWinner()
+                    self.getEmptyFields()
+                    otpt = True
+                else:
+                    if self.verbose: print("doMove: game over")
+            else:
+                if self.verbose: print("doMove: field is already taken")
         else:
-            move = minus_player_func(board_state, -1)
+            if self.verbose: print("doMove: Its not player " + player + "'s turn")
+        return otpt
 
-        if move not in _available_moves:
-            # if a player makes an invalid move the other player wins
-            if log:
-                print("illegal move ", move)
-            return -player_turn
+    def randomPlayer2(self):
+        if self.p2 == self.currentPlayer:
+            if not self.gameOver:
+                availableFields = self.getEmptyFields()
+                randArray = np.random.choice(len(availableFields), 1)
+                randAction = availableFields[randArray.item()]
+                self.doMove(randAction,self.p2)
+            else:
+                if self.verbose: print("game is over")
+        else:
+            if self.verbose: print("Its p1's turn, p2 cannot move now")
+    
+    def resetSim(self):
+        self.resetBoard()
+        if self.currentPlayer == self.p2:
+            self.randomPlayer2()
+        return self.stateHash2Num[self.getState()]
+            
+            
+    def doSimMove(self,field):
+        if not self.gameOver:
+            if self.p1 == self.currentPlayer:
+                ok = self.doMove(field,self.p1)
+                self.getWinner()
+                if ok:
+                    if not self.gameOver:
+                        self.randomPlayer2()
+                        self.getWinner()
+        return self.gameOver,self.stateHash2Num[self.getState()],self.rewardVal 
+                
 
-        board_state = apply_move(board_state, move, player_turn)
-        print(board_state)
-
-        winner = has_winner(board_state, winning_length)
-        if winner != 0:
-            if log:
-                print("we have a winner, side: %s" % player_turn)
-            return winner
-        player_turn = -player_turn
 
 
-def random_player(board_state, _):
-    """A player func that can be used in the play_game method. Given a board state it chooses a move randomly from the
-    valid moves in the current state.
-    Args:
-        board_state (2d tuple of int): The current state of the board
-        _: the side this player is playing, not used in this function because we are simply choosing the moves randomly
-    Returns:
-        (int, int): the move we want to play on the current board
-    """
-    moves = list(available_moves(board_state))
-    return random.choice(moves)
+"""        
+env = tictactoe(3,'p1','p2',False,False)
 
- 
-if __name__ == '__main__':
-    # example of playing a game
-    play_game(random_player, random_player, log=True, board_size=3, winning_length=3)
+s =env.resetSim()
+
+gameOverTrue = env.gameOver
+counter = 0
+
+actSpace = env.actionSpace
+sizeActionSpace = len(actSpacte)
+sizeStateSpace = len(env.stateSpace)
+
+
+tf.reset_default_graph()
+
+inputs1 = tf.placeholder(shape=[1,sizeStateSpace],dtype=tf.float32)
+W = tf.Variable(tf.random_uniform([sizeStateSpace,sizeActionSpace],0,0.01))
+Qout = tf.matmul(inputs1,W)
+predict = tf.argmax(Qout,1)
+
+nextQ = tf.placeholder(shape=[1,sizeActionSpace],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+# Set learning parameters
+y = .99
+e = 0.1
+num_episodes = 500
+#create lists to contain total rewards and steps per episode
+jList = []
+rList = []
+with tf.Session() as sess:
+    sess.run(init)
+    saver = tf.train.Saver()
+    for i in range(num_episodes):
+        #Reset environment and get first new observation
+        if i % 10 == 0:
+            print("This is episode: " + str(i).rjust(5))
+        s = env.resetSim()
+        rAll = 0
+        d = False
+        j = 0
+        #The Q-Network
+        while j < 99:
+            j+=1
+            #Choose an action by greedily (with e chance of random action) from the Q-network
+            a,allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(sizeStateSpace)[s:s+1]})
+            if np.random.rand(1) < e:
+                a[0] = rnd.choice(range(sizeActionSpace))
+            #Get new state and reward from environment
+            d,s1,r = env.doSimMove(actSpace[a[0]])
+            #Obtain the Q' values by feeding the new state through our network
+            Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(sizeStateSpace)[s1:s1+1]})
+            #Obtain maxQ' and set our target value for chosen action.
+            maxQ1 = np.max(Q1)
+            targetQ = allQ
+            targetQ[0,actSpace[a[0]]] = r + y*maxQ1
+            #Train our network using target and predicted Q values
+            _,W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(sizeStateSpace)[s:s+1],nextQ:targetQ})
+            rAll += r
+            s = s1
+            if d == True:
+                #Reduce chance of random action as we train the model.
+                e = 1./((i/50) + 10)
+                break
+        jList.append(j)
+        rList.append(rAll)
+    saver.save(sess, "saved_models/testing_si")
+    print("Model Saved.")
+        
+plt.plot(rList)
+plt.plot(jList)
+"""
+
